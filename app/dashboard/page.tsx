0d2d6a95c82735d4171f8634a9dab077e7f1da32
Isaac Dolphin <isaac_dolphin@outlook.com>
2025-03-09 23:53:29 +0000

    Implemented sign out functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { signOut } from "../auth/actions";
+
 export default function DashboardPage() {
-    return <h1>Dashboard Page</h1>
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <Button onClick={signOut}>Sign Out</Button>
+    </>
+  );
 }
