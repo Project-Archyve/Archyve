e9d2424559eeeddef8e1274556d956321e0bf39b
Isaac Dolphin <isaac_dolphin@outlook.com>
2025-03-10 00:16:11 +0000

    Added UserProvider.tsx with appropriate logic

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,16 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { signOut } from "../auth/actions";
+import { useUser } from "@/hooks/UserProvider";
 
 export default function DashboardPage() {
+
+  const user = useUser();
+
   return (
     <>
-      <h1>Dashboard Page</h1>
+      <h1>Welcome to the Dashboard {user?.email}</h1>
       <Button onClick={signOut}>Sign Out</Button>
     </>
   );
