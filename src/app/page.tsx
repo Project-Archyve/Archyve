32957d978d8b286e554ef6f1e4c750acc4e1bb8b
Isaac Dolphin <d044286m@student.staffs.ac.uk>
2025-04-28 13:34:08 +0100

    brief changes to merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/dashboard/3e539dba-160a-4ff3-bf08-000d90381278")
-  {/* option to create org or view the dashboard of an existing one. */}
+  redirect("/dashboard")
 }
